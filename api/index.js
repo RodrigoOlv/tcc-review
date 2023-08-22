a2eef67ff6e34792e2fdee04c0cf37fce8ee56da
Rodrigo Oliveira <rodrigo@santins.com.br>
2023-08-22 00:52:08 -0300

    separação partial do texto em partes para envio ao openai

--- api/index.js
+++ api/index.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const MAX_TOKENS_PER_REQUEST = 4000; // Limite máximo de tokens por solicitação
+
 app.post('/document', async (req, res) => {
   try {
     const url = req.body.url;
@@ -22,18 +24,23 @@ app.post('/document', async (req, res) => {
     let analysis = null;
     let missingReferences = null;
 
-    analysis = await analyzeText(content);
+    // Divide o conteúdo em partes menores para não exceder o limite de tokens
+    const contentChunks = splitTextIntoChunks(content, MAX_TOKENS_PER_REQUEST);
 
-    if (options.validateSummary) {
-      summary = await validateSummary(content);
-    }
+    for (const chunk of contentChunks) {
+      analysis = await analyzeText(chunk);
 
-    if (options.findKeywords) {
-      keywords = await findKeywords(content);
-    }
+      if (options.validateSummary) {
+        summary = await validateSummary(chunk);
+      }
 
-    if (options.checkReferences) {
-      missingReferences = validateReferences(content); // Use a função do arquivo separado
+      if (options.findKeywords) {
+        keywords = await findKeywords(chunk);
+      }
+
+      if (options.checkReferences) {
+        missingReferences = validateReferences(chunk); // Use a função do arquivo separado
+      }
     }
 
     const responseObj = {
@@ -54,3 +61,26 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`);
 });
+
+// Função para dividir o texto em partes menores
+const splitTextIntoChunks = (text, maxTokens) => {
+  const chunks = [];
+  const words = text.split(' ');
+  let currentChunk = '';
+
+  for (const word of words) {
+    const wordTokens = word.split(' ').length;
+    if ((currentChunk.length + wordTokens) <= maxTokens) {
+      currentChunk += ` ${word}`;
+    } else {
+      chunks.push(currentChunk.trim());
+      currentChunk = `${word}`;
+    }
+  }
+
+  if (currentChunk.length > 0) {
+    chunks.push(currentChunk.trim());
+  }
+
+  return chunks;
+};
