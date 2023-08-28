2de76cc1bc471ac57f27072bb980322619026298
Rodrigo Oliveira <rodrigo@santins.com.br>
2023-08-28 16:03:13 -0300

    Envio fragmentado de texto longo para a análise bloco a bloco, formatação e iteração no front

--- api/controllers/chatGPT.js
+++ api/controllers/chatGPT.js
@@ -1,32 +1,99 @@
 const axios = require('axios');
 require('dotenv').config();
 
+// Função para dividir o texto em partes menores
+const splitTextIntoChunks = (text, maxTokens) => {
+  const chunks = [];
+  const words = text.split(' ');
+  let currentChunk = '';
+
+  for (const word of words) {
+    const wordTokens = word.split(' ').length;
+    if ((currentChunk.length + wordTokens) <= maxTokens) {
+      currentChunk += ` ${word}`;
+    } else {
+      chunks.push(currentChunk.trim());
+      currentChunk = `${word}`;
+    }
+  }
+
+  if (currentChunk.length > 0) {
+    chunks.push(currentChunk.trim());
+  }
+
+  return chunks;
+};
+
+// Estado inicial do contexto
+let conversationContext = [
+  { role: 'system', content: 'Você é um assistente útil que analisa texto.' },
+  { role: 'user', content: 'Início do texto a ser analisado:' },
+];
+
+const cleanAndFormatResponse = (response) => {
+  // Remova o identificador da parte [Parte X] e os colchetes iniciais usando expressão regular
+  const cleanedResponse = response.replace(/\[Parte \d+\]/g, '').replace(/^"|"$/g, '');
+
+  return cleanedResponse;
+};
+
 const analyzeText = async (message) => {
   try {
     const apiUrl = 'https://api.openai.com/v1/chat/completions';
     const apiKey = process.env.OPENAI_API_KEY;
 
-    const response = await axios.post(apiUrl, {
-      messages: [
-        { role: 'system', content: 'You are a helpful assistant that analyzes text.' },
-        { role: 'user', content: `Analise a linguagem, estrutura e conteúdo do seguinte texto: ${message}` }
-      ],
-      model: 'gpt-3.5-turbo',
-      temperature: 0.5,
-      max_tokens: 400,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-      },
-    });
+    const parts = splitTextIntoChunks(message, 2000);
 
-    return response.data.choices[0].message.content;
+    const getConversationContext = (partIndex) => {
+      const context = [
+        { role: 'system', content: 'Você é um assistente útil que analisa texto.' },
+        { role: 'user', content: 'Início do texto a ser analisado (se não puder analisar, envie uma mensagem em branco):' },
+        { role: 'user', content: `[Parte ${partIndex + 1}] ${parts[partIndex]}` },
+        { role: 'user', content: 'Continuação da análise:' },
+      ];
+
+      return context;
+    };
+
+    let conversationContext = getConversationContext(0);
+    const assistantResponses = [];
+
+    for (let i = 0; i < parts.length; i++) {
+      const response = await axios.post(apiUrl, {
+        messages: conversationContext,
+        model: 'gpt-3.5-turbo',
+        temperature: 0.5,
+        max_tokens: 2000,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      });
+
+      const assistantMessage = response.data.choices[0].message.content;
+      
+      // Limpe e formate a resposta antes de armazená-la
+      const formattedResponse = cleanAndFormatResponse(assistantMessage);
+
+      assistantResponses.push(formattedResponse);
+
+      // Atualize o contexto apenas se houver mais partes a serem processadas
+      if (i < parts.length - 1) {
+        conversationContext = [
+          ...getConversationContext(i + 1),
+          { role: 'assistant', content: assistantMessage },
+        ];
+      }
+    }
+
+    return assistantResponses;
   } catch (error) {
     console.log('Erro ao realizar a análise de texto:', error);
     throw error;
   }
 };
+
 
 const validateSummary = async (message) => {
   try {
@@ -82,4 +149,4 @@ const findKeywords = async (message) => {
   }
 };
 
-module.exports = { analyzeText, validateSummary, findKeywords };
+module.exports = { splitTextIntoChunks, analyzeText, validateSummary, findKeywords };
--- api/index.js
+++ api/index.js
@@ -3,14 +3,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const { getDocumentContent, extractGoogleDocsId } = require('./controllers/google');
-const { analyzeText, validateSummary, findKeywords } = require('./controllers/chatGPT');
+const { splitTextIntoChunks, analyzeText, validateSummary, findKeywords } = require('./controllers/chatGPT');
 const { validateReferences } = require('./referenceValidator'); // Importe a função
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-const MAX_TOKENS_PER_REQUEST = 4000; // Limite máximo de tokens por solicitação
 
 app.post('/document', async (req, res) => {
   try {
@@ -23,26 +21,21 @@ app.post('/document', async (req, res) => {
     let keywords = null;
     let analysis = null;
     let missingReferences = null;
+    
+    analysis = await analyzeText(content);
 
-    // Divide o conteúdo em partes menores para não exceder o limite de tokens
-    const contentChunks = splitTextIntoChunks(content, MAX_TOKENS_PER_REQUEST);
-
-    for (const chunk of contentChunks) {
-      analysis = await analyzeText(chunk);
-
-      if (options.validateSummary) {
-        summary = await validateSummary(chunk);
-      }
-
-      if (options.findKeywords) {
-        keywords = await findKeywords(chunk);
-      }
-
-      if (options.checkReferences) {
-        missingReferences = validateReferences(chunk); // Use a função do arquivo separado
-      }
+    if (options.validateSummary) {
+      summary = await validateSummary(content);
     }
 
+    if (options.findKeywords) {
+      keywords = await findKeywords(content);
+    }
+
+    if (options.checkReferences) {
+      missingReferences = validateReferences(content); // Use a função do arquivo separado
+    }
+    
     const responseObj = {
       analysis: analysis,
       summary: summary,
@@ -62,25 +55,3 @@ app.listen(port, () => {
   console.log(`API rodando na porta ${port}`);
 });
 
-// Função para dividir o texto em partes menores
-const splitTextIntoChunks = (text, maxTokens) => {
-  const chunks = [];
-  const words = text.split(' ');
-  let currentChunk = '';
-
-  for (const word of words) {
-    const wordTokens = word.split(' ').length;
-    if ((currentChunk.length + wordTokens) <= maxTokens) {
-      currentChunk += ` ${word}`;
-    } else {
-      chunks.push(currentChunk.trim());
-      currentChunk = `${word}`;
-    }
-  }
-
-  if (currentChunk.length > 0) {
-    chunks.push(currentChunk.trim());
-  }
-
-  return chunks;
-};
